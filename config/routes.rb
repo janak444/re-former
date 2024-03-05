4b1a55188c38101e7a55aabaaca5e95f02dd66b3
YOUR NAME <YOUR@EMAIL.com>
2024-03-05 16:51:05 +0545

    Clarify the edit, show and update in database

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: %i[new create edit]
+  resources :users, only: %i[new create show edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
